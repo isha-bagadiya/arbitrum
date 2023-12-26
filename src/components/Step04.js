5962b5b6d64072d72426bcef59e6e3746b9a8f17
Jaydip-lamprostech <jaydip.lamprostech@gmail.com>
2023-12-26 17:19:33 +0530

    update google recaptcha code

--- src/components/Step04.js
+++ src/components/Step04.js
@@ -11,7 +11,8 @@ function Step03({ addressInput, setShowConfetti, showConfetti }) {
   const [receipt, setReceipt] = useState("");
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [showLowBalanceMsg, setLowBalanceMsg] = useState(true);
+  const [showLowBalanceMsg, setLowBalanceMsg] = useState(false);
+  const [captchaToken, setCaptchaToken] = useState("");
   const captchaRef = useRef(null);
 
   const { Send } = DiscordService();
@@ -35,7 +36,7 @@ function Step03({ addressInput, setShowConfetti, showConfetti }) {
         console.log("Error:", error.message);
       }
     };
-    fetchBalance();
+    // fetchBalance();
   }, []);
 
   useEffect(() => {
@@ -48,9 +49,8 @@ function Step03({ addressInput, setShowConfetti, showConfetti }) {
 
   const handleDrip = async () => {
     setLoading(true);
-    const token = captchaRef.current.getValue();
     const inputVal = "";
-    captchaRef.current.reset();
+    const token = captchaToken;
     try {
       // Make a POST request to your API endpoint
       const humanCheck = await fetch(
@@ -68,39 +68,41 @@ function Step03({ addressInput, setShowConfetti, showConfetti }) {
 
       // Update the result state based on the API response
       console.log(data);
-      const response = await fetch("https://modedomains.vercel.app/drip", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ recipientAddress }),
-      });
+      if (data) {
+        const response = await fetch("https://modedomains.vercel.app/drip", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ recipientAddress }),
+        });
 
-      if (response.ok) {
-        // discord msg
-        Send(
-          ` 🎉 Congratulations! ${
-            recipientAddress.slice(0, 7) +
-            "..." +
-            recipientAddress.slice(
-              recipientAddress.length - 5,
-              recipientAddress.length
-            )
-          } just claimed the faucet 0.03 ETH ! 🎉`
-        );
+        if (response.ok) {
+          // discord msg
+          Send(
+            ` 🎉 Congratulations! ${
+              recipientAddress.slice(0, 7) +
+              "..." +
+              recipientAddress.slice(
+                recipientAddress.length - 5,
+                recipientAddress.length
+              )
+            } just claimed the faucet 0.03 ETH ! 🎉`
+          );
 
-        setLoading(false);
-        const data = await response.json();
-        setMessage(data.message);
-        setReceipt(data.receipt);
-        setShowConfetti(true);
-        setError(null);
-      } else {
-        setLoading(false);
-        const errorData = await response.json();
-        setError(errorData.message);
-        setMessage("");
-        setReceipt("");
+          setLoading(false);
+          const data = await response.json();
+          setMessage(data.message);
+          setReceipt(data.receipt);
+          setShowConfetti(true);
+          setError(null);
+        } else {
+          setLoading(false);
+          const errorData = await response.json();
+          setError(errorData.message);
+          setMessage("");
+          setReceipt("");
+        }
       }
     } catch (err) {
       setLoading(false);
@@ -108,6 +110,14 @@ function Step03({ addressInput, setShowConfetti, showConfetti }) {
       setMessage("");
       setReceipt("");
     }
+    // finally {
+    //   setCaptchaToken("");
+    // }
+  };
+
+  const handleCaptchaChange = (value) => {
+    // console.log(value);
+    setCaptchaToken(value);
   };
   return (
     <div className="step_1">
@@ -117,11 +127,18 @@ function Step03({ addressInput, setShowConfetti, showConfetti }) {
       <ReCAPTCHA
         sitekey={process.env.REACT_APP_SITE_KEY}
         ref={captchaRef}
-        className={showLowBalanceMsg ? "disabled" : ""}
+        onChange={handleCaptchaChange}
+        theme="dark"
+        className={showLowBalanceMsg ? "disabled recaptcha" : "recaptcha"}
       />
+      {!captchaToken ? (
+        <p className="error_msg">
+          Complete the captcha to claim faucet rewards.
+        </p>
+      ) : null}
       <button
         className={
-          showConfetti || showLowBalanceMsg
+          showConfetti || showLowBalanceMsg || !captchaToken
             ? "claim-faucet disabled"
             : "claim-faucet"
         }
